022ecf310086983d371ee93ea822029a4e974267
Johan Yngman <johan.yngman@gmail.com>
2024-06-16 20:06:11 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+cmake -S src/ -B build -G "Ninja Multi-Config"
+cmake --build build --config Release
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "CI/CD config for GitHub Actions";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        dependencies = with pkgs; [ bash coreutils clang cmake ninja ];
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "template";
+          buildInputs = dependencies;
+          src = ./.;
+          dontUseCmakeConfigure = true;
+          buildPhase = ''
+            export CC=${pkgs.clang}/bin/clang
+            export CXX=${pkgs.clang}/bin/clang++
+            $src/build.sh
+          '';
+          installPhase = "cmake --install build --prefix $out";
+        };
+        # packages.image = pkgs.dockerTools.buildLayeredImage {
+        #   name = "template-image";
+        #   tag = "latest";
+        #   contents = dependencies;
+        #   config.cmd 
+        # };
+        devShells.default = pkgs.mkShell { packages = dependencies; };
+      });
+}
